a9b68de2154d3106700e51b1a56a5410f5917666
Spencer Wiese <swiese12@huskers.unl.edu>
2022-01-28 15:43:31 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Spencer Wiese
+ *         swiese12@huskers.unl.edu
+ * Date: 2022/01/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Spencer Wiese\n");
+
+  return 0;
+}
